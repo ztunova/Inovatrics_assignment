4c1519661094a81c00b2d4a0ff0c8ca611cc2c79
Zofia Tunova <zofka.tunova@gmail.com>
2022-10-10 12:51:24 +0200

    basic directory controller

--- src/main/java/com/inovatrics/rest_api_proj/DirectoryController.java
+++ src/main/java/com/inovatrics/rest_api_proj/DirectoryController.java
@@ -3,15 +3,13 @@ package com.inovatrics.rest_api_proj;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.*;
 
 @RestController
 public class DirectoryController {
 
     @PostMapping("directory")
     public void createNewDirectory(@RequestParam("path") String dirPath){
-       // Path path = Paths.get(dirPath);
         File newDirectory = new File(dirPath);
         if (!newDirectory.exists()){
 
@@ -25,14 +23,71 @@ public class DirectoryController {
         }
     }
 
-    @DeleteMapping("/directory/{path}")
-    public void deleteExistingDirectory(@PathVariable("path") String path){}
+    private void deleteSubfiles(File folder){
+        File[] childFolders = folder.listFiles();
 
-    @GetMapping("/directory/{path}")
-    public void listContentOfDirectory(@PathVariable("path") String path){
-        //ordered by size
+        assert childFolders != null;
+        for (File file : childFolders){
+            if (file.isDirectory()){
+                deleteSubfiles(file);
+            }
+            file.delete();
+        }
     }
 
+    @DeleteMapping("directory")
+    public void deleteExistingDirectory(@RequestParam("path") String dirPath){
+        File dir = new File(dirPath);
+        deleteSubfiles(dir);
+        dir.delete();
+    }
 
+    private long lengthOfDirectory(File folder){
+        File[] filesInFolder = folder.listFiles();
+        long size = 0;
+        assert filesInFolder != null;
+        for (File f : filesInFolder){
+            if (f.isFile()){
+                size = size + f.length();
+            } else {
+                size = size + lengthOfDirectory(f);
+            }
+        }
+        System.out.println("size of folder: " + size);
+        return size;
+    }
 
+    @GetMapping("directory")
+    public void listContentOfDirectory(@RequestParam("path") String dirPath){
+        //TODO response
+        File dir = new File(dirPath);
+        File[] fileArr = dir.listFiles();
+        if (fileArr != null) {
+            TreeMap<Long, ArrayList<String>> sizeFileMap = new TreeMap<>();
+
+            long size = 0;
+            ArrayList<String> value;
+            for (File f : fileArr){
+                System.out.println(f.getName());
+                if (f.isDirectory()){
+                    System.out.println("directory");
+                    size = lengthOfDirectory(f);
+                } else {
+                    size = f.length();
+                }
+
+                if (sizeFileMap.containsKey(size)){
+                    value = sizeFileMap.get(size);
+                } else {
+                    value = new ArrayList<String>();
+                }
+                value.add(f.getName());
+                sizeFileMap.put(size, value);
+            }
+
+            System.out.println(sizeFileMap);
+        } else {
+            System.out.println("empty directory");
+        }
+    }
 }
